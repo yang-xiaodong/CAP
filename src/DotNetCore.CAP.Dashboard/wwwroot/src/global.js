dbdfe7ad3845c383833fbb0181671c441c06ae0c
Savorboard <yangxiaodong1214@126.com>
2021-06-09 11:46:38 +0800

    trigger to test github action.

--- src/DotNetCore.CAP.Dashboard/wwwroot/src/global.js
+++ src/DotNetCore.CAP.Dashboard/wwwroot/src/global.js
@@ -2,7 +2,7 @@ let baseURL = "";
 
 switch (process.env.NODE_ENV) {
     case 'development':
-        baseURL = 'http://localhost:5001';
+        baseURL = 'http://localhost:5000';
         break
     default:
         baseURL = window.serverUrl;
